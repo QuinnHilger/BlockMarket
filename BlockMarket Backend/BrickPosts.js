b222dba69f26393070373b8b0fee693cc880df25
LeothePold <97650088+LeothePold@users.noreply.github.com>
2022-11-28 18:08:50 -0800

    Add files via upload

--- BlockMarket Backend/BrickPosts.js
+++ BlockMarket Backend/BrickPosts.js
@@ -48,8 +48,31 @@ PostSchema.statics.findByTitle = function(title) {
     return this.where({ title: new RegExp(title, "i")})
 }
 
+PostSchema.statics.findByPrice = function(price) {
+    return this.where({ price: new RegExp(price, "i")})
+}
+
 PostSchema.statics.findByDescription = function(description) {
     return this.where({ description: new RegExp(description, "i")})
 }
+
+PostSchema.statics.findByTheme = function(theme) {
+    return this.where({ theme: new RegExp(theme, "i")})
+}
+
+PostSchema.statics.findByPieces = function(pieces) {
+    return this.where({ pieces: new RegExp(pieces, "i")}) . //MAY NEED TO CHANGE THIS DEPENDING ON WHETHER WE WANT TO SEARCH BY MIN PIECES, MAX PIECES, OR EXACT PIECES(CURRENT IMPLEMENTATION)
+}
+
+PostSchema.statics.findByCondition = function(condition) {
+    return this.where({ condition: new RegExp(condition, "i")}) //FOR THE IMPLEMENATION OF THIS SCHEMA, BEST JUST TO IGNORE THE PICTURE OF THE CONDITION CLASS, AND JUST PROCEED IN THE SAME MANNER AS THE OTHER SCHEMA
+}
+
+PostSchema.statics.findByReview = function(review) {
+    return this.where({ review: new RegExp(review, "i")})
+}
+
+//DIDNT THINK WE NEEDED A SCHEMA FOR "CREATED AT, OPEN TO CREATING IT IF NEEDED HOWEVER"
+
 
 module.exports = mongoose.model("Post", PostSchema)
